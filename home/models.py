4d51d583e6fcd292d614c8575e6976d17cb33177
Jerod Estapa <jerodestapa@gmail.com>
2017-03-14 23:53:59 -0500

    Created blog index page template

--- home/models.py
+++ home/models.py
@@ -1,14 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 
-from django.db import models
-
-from modelcluster.fields import ParentalKey
-
 from wagtail.wagtailcore.models import Page
-from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
-from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
-from wagtail.wagtailsearch import index
 from blog.models import BlogPage
 
 
