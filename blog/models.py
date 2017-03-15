168cc3df2c916e8d52100f62c7bcca4f2d2b2860
Jerod Estapa <jerodestapa@gmail.com>
2017-03-14 23:35:59 -0500

    Added BlogIndexPage model

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from modelcluster.fields import ParentalKey
 
 from wagtail.wagtailcore.models import Page, Orderable
-from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailcore.fields import RichTextField, Paginator, EmptyPage, PageNotAnInteger
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailsearch import index
@@ -56,9 +56,58 @@ class BlogPageRelatedLink(Orderable):
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
+    search_fields = Page.search_fields + [
+        index.SearchField('intro')
+    ]
+
+    @property
+    def blogs(self):
+        # Get list of live blog pages that are descendents of this page
+        blogs = BlogPage.objects.live().descendent_of(self)
+
+        # Order by most recent date
+        blogs = blogs.order_by('date')
+
+        return blogs
+
     def get_context(self, request):
-        # Update to include only published posts, ordered by reverse-chron
+        # Get blogs
+        blogs = self.blogs
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        if tag:
+            blogs = blogs.filter(tags__name=tag)
+
+        # Pagination
+        page = request.GET.get('page')
+        paginator = Paginator(blogs, 5)  # Show 5 blogs per page
+        try:
+            blogs = paginator.page(page)
+        except PageNotAnInteger:
+            blogs = paginator.page(1)
+        except EmptyPage:
+            blogs = paginator.page(paginator.num_pages)
+
+        # Update template context
         context = super(BlogIndexPage, self).get_context(request)
-        blogpages = self.get_children().live().order_by('-first_published_at')
-        context['blogpages'] = blogpages
+        context['blogs'] = blogs
         return context
+
+    def get_sitemap_urls(self):
+        return [
+            {
+                'location': self.full_url,
+                'lastmod': self.latest_revision_created_at,
+                'changefreq': 'monthly',
+                'priority': .5
+            }
+        ]
+
+    content_panels = [
+        FieldPanel('title', classname="full title"),
+        FieldPanel('intro', classname="full"),
+        InlinePanel('related_links', label="Related links"),
+    ]
+
+    promote_panels = Page.promote_panels
